d1c7465c911b198f34adffdbce9bba432b93eabb
Harry Bond-Preston <harrytbp@gmail.com>
2019-03-13 16:54:56 +0000

    Moved config variables to new file

--- config/__init__.py
+++ config/__init__.py
@@ -5,15 +5,15 @@ Creates the root directory and defines computation device (cpu/cuda)
 import os
 from pathlib import Path
 import torch
+from .variables import BASE_DIRECTORY
 
 '''
 DEVICE:             if you do not have CUDA then DEVICE must be set to cpu
-BASE DIRECTORY:     change this to your chosen directory
+BASE DIRECTORY:     change this to your chosen dpythirectory
 '''
 DEVICE = torch.device('cpu')
 if torch.cuda.is_available():
     DEVICE = torch.device('cuda')
-BASE_DIRECTORY = str(Path.home()) + '/texture_generation'
 
 if not os.path.exists(BASE_DIRECTORY):
     os.makedirs(BASE_DIRECTORY)
---
+++ config/variables.py
@@ -0,0 +1,3 @@
+from pathlib import Path
+
+BASE_DIRECTORY = str(Path.home()) + '/texture_generation'
